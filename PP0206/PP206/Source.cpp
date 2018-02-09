7fd61523c0e99490f8905448f904f06a5f3397c3
oceanmaniacs <34010621+oceanmaniacs@users.noreply.github.com>
2018-02-09 09:02:16 -0600

    Add files via upload

---
+++ PP0206/PP206/Source.cpp
@@ -0,0 +1,20 @@
+//Yet another well-adjusted printing program
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+
+	int dummy;
+	cout << "The following iteams were top sellers\n";
+	cout << "during the month of June:\n";
+	cout << "Computer games \nCoffee";
+	cout << "\nAspirin\n";
+
+	cin >> dummy;
+	return 0;
+
+
+
+}
