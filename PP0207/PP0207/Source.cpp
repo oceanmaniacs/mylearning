570383346eae34c5f540ad178b40b1ee1d31f7ed
Wellington Tejada <34010621+oceanmaniacs@users.noreply.github.com>
2018-02-10 16:51:04 -0600

    starting out with c++
    programming I

---
+++ PP0207/PP0207/Source.cpp
@@ -0,0 +1,20 @@
+//This program has a variable
+#include <iostream>
+using namespace std;
+
+int main()
+
+
+{
+	int dummy;
+
+	int number;
+
+	number = 5;
+	cout << "The value in number is " << number << endl;
+
+	cin >> dummy;
+	return 0;
+
+
+}
---
+++ PP0208/PP0208/Source.cpp
@@ -0,0 +1,21 @@
+//This program has a variable
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+
+	int dummy;
+
+	int number;
+	number = 5;
+
+	cout << "The value of number is " << "number" << endl;
+
+	cin >> dummy;
+	return 0;
+
+
+
+}
